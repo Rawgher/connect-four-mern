53b8d1ba7331d2bb0533d5c4f2cf88b1d86a7c12
Roger Pender <rdpender3@gmail.com>
2023-04-17 15:41:49 -0700

    combining two files

---
+++ connect-four-backend/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+async function mongoConnect() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+mongoConnect();
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+const user = new User({
+  username: 'test',
+  password: 'test1',
+});
+
+user.save();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.send("The game's server is working");
+});
